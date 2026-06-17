0bc7573499d4f5b45a0768bb6bae2f5004933d2b
vikash <vikashkelly@gmail.com>
2026-06-17 10:58:28 +0530

    feat: implement root layout with site metadata, global components, and font integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..700;1,400..700&family=Poppins:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
